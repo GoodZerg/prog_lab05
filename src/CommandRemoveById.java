aebca1054703ee4fea698cd8b6408cbe801702f9
[ sora. ] <57730505+GoodZerg@users.noreply.github.com>
2023-03-30 01:39:31 +0300

    remove commands(By id not debbuging)

--- src/CommandRemoveById.java
+++ src/CommandRemoveById.java
@@ -1,12 +1,24 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class CommandRemoveById extends Command{
-    private Long id;
-    CommandRemoveById(DeqCollection<?> data, Long id) {
+    private long id;
+    CommandRemoveById(DeqCollection<?> data, long id) {
         super(data);
         this.id = id;
     }
-
+    private <T extends Collectible & Comparable<T>> void fooHelper(DeqCollection<T> data){
+        T[] arr = data.getStorage().toArray(data.createContentsArray(data.getStorage().size()));
+        for (T i : arr) {
+            if(i.getId() == id){
+                data.getStorage().remove(i);
+                return;
+            }
+        }
+    }
     @Override
     public void execute() {
-
+        fooHelper(data);
     }
 }
--- src/CommandRemoveLower.java
+++ src/CommandRemoveLower.java
@@ -1,10 +1,23 @@
+import java.util.Optional;
+
 public class CommandRemoveLower extends Command{
     CommandRemoveLower(DeqCollection<?> data) {
         super(data);
     }
+    private <T extends Collectible & Comparable<T>> void fooHelper(DeqCollection<T> data){
+        T[] arr = data.getStorage().toArray(data.createContentsArray(data.getStorage().size()));
 
+        T tmp = (T)data.createContents();
+        tmp.loadFromStandardInput();
+
+        for (T i : arr) {
+            if(tmp.compareTo(i) > 0){
+                data.getStorage().remove(i);
+            }
+        }
+    }
     @Override
     public void execute() {
-
+        fooHelper(data);
     }
 }
